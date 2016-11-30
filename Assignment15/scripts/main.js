a398e016c2e9be2e078f0992397cd5c1ffc42d7b
jessi0214 <jessi_enyart@yahoo.com>
2016-11-29 19:12:57 -0600

    1st Jquery assignment

--- Assignment15/scripts/main.js
+++ Assignment15/scripts/main.js
@@ -2,9 +2,32 @@ $(document).ready(function(){
 
      ///// Code goes here
 
+ $('#btnOne').on('click', function (){
+   alert('hello');
+ });
+
+  $('#btnTwo').on('click', function(){
+    $(".update-html").html('Hello World')
+
+  });
+
+  $('#btnThree').on('click', function(){
+    $("#update-html").val('Hello World')
+
+  });
+
+  $('#btnCopyHtml').on('click', function(){
+    var html = $('.copy-html').html()
+    $('.paste-html').html(html)
+
+  });
+  $('#btnCopyValues').on('click', function(){
+    var html = $('.copy-html').val()
+    $('.copiedvalue').html(html)
+      $("paste-text").val('copiedValue')  
 
 
 
 
 
-});
+  });
